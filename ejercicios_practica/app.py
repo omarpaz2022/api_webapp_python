8374422c49eff2125c5f50789b98b4fcaf67561f
omarpaz2022 <107511393+omarpaz2022@users.noreply.github.com>
2023-05-23 12:48:04 -0300

    ejercicio de practica resuelto

--- ejercicios_practica/app.py
+++ ejercicios_practica/app.py
@@ -50,19 +50,34 @@ def personas():
         # limit = ...
         # offset = ....
 
+        # Obtener de la query string los valores de limit y offset
+        limit_str = str(request.args.get('limit'))
+        offset_str = str(request.args.get('offset'))
+        limit = 0
+        offset = 0
+
         # Debe verificar si el limit y offset son válidos cuando
         # no son especificados en la URL
+        if(limit_str is not None) and (limit_str.isdigit()):
+            limit = int(limit_str)
+        if(offset_str is not None) and (offset_str.isdigit()):
+            offset = int(offset_str)
 
         # Alumno: Pasarle al metodo report los valores de limit y offset
-        data = persona.report()
+        data = persona.report(limit=limit, offset=offset)
         
-        result = '''<h3>Alumno: Implementar la llamada
-                    al HTML tabla.html
-                    con render_template, recuerde pasar
-                    data como parámetro</h3>'''
-        # Sacar esta linea cuando haya implementado el return
-        # con render template
-        return result
+        # Renderizar el template HTML tabla.html
+        print("Renderizar tabla.html")
+        return render_template('tabla.html', data=data)
+     
+        # result = '''<h3>Alumno: Implementar la llamada
+        #             al HTML tabla.html
+        #             con render_template, recuerde pasar
+        #             data como parámetro</h3>'''
+        # # Sacar esta linea cuando haya implementado el return
+        # # con render template
+        # return result
+
     except:
         return jsonify({'trace': traceback.format_exc()})
 
@@ -81,19 +96,24 @@ def registro():
             name = ""
             age = 0
 
-            return "Alumno --> Realice la implementacion y borre este return"
+            # return "Alumno --> Realice la implementacion y borre este return"
 
             # Alumno:
             # Obtener del HTTP POST JSON el nombre y la edad
             # name = ...
             # age = ...
+            name = str(request.form.get("name"))
+            age = str(request.form.get("age"))
+            if(name is None or age is None or age.isdigit() is False):
+                # Datos ingresados incorrectos
+                    return Response(status=400)
 
             # Alumno: descomentar la linea persona.insert una vez implementado
             # lo anterior:
-            # persona.insert(name, int(age))
+            persona.insert(name, int(age))
             
             # Como respuesta al POST devolvemos la tabla de valores
-            # return redirect(url_for('personas'))
+            return redirect(url_for("personas"))
         except:
             return jsonify({'trace': traceback.format_exc()})
 
@@ -114,12 +134,11 @@ def comparativa():
         # todas las edades respectivas a los Ids que se encuentran en "x"
 
         # Descomentar luego de haber implementado su función en persona.py:
-
-        # x, y = persona.dashboard()
-        # image_html = utils.graficar(x, y)
-        # return Response(image_html.getvalue(), mimetype='image/png')
-
-        return "Alumno --> Realice la implementacion"
+         x, y = persona.dashboard()
+         image_html = utils.graficar(x, y)
+         return Response(image_html.getvalue(), mimetype='image/png')
+        #return "Alumno --> Realice la implementacion"
+        
     except:
         return jsonify({'trace': traceback.format_exc()})
 
